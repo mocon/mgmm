8223c4186d3c4558de39868f5bde3de9b226be37
myles <myles@Myles-OConnor-iMac-27.local>
2016-07-20 13:42:38 -0700

    Progress on layout of Review Report view

--- app/components/Review.js
+++ app/components/Review.js
@@ -7,11 +7,78 @@ var Review = React.createClass({
         document.body.className = '';
     },
     render: function() {
+        let reportType = "Advertising";
+
         return (
             <div>
-                <section className="main-content">
-                    <p>Review report goes here</p>
+                {/* Report title section, needs [client name], [report subtitle], & [report type] */}
+                <section className="-p-v-6 -p-h-3 bg--trans-dark-1">
+                    <div className="gds-layout__container">
+                        <div className="gds-layout__row">
+                            <div className="gds-layout__column--sm-12 gds-layout__hidden-md-up -text-center -m-b-4">
+                                <img className="img--full-width" src="https://c.gumgum.com/ads/com/mgmm/img/svg/gg-vi--color.svg" alt="GumGum Visual Intelligence" />
+                            </div>
+                            <div className="gds-layout__column--md-8 -text-center--sm">
+                                <p className="-block text--gg-gold text--uppercase">[{reportType}] audit for</p>
+                                <h1 className="gds-text--header-lg gds-text--bold -block text--white">[Report Client Name] goes here</h1>
+                                <h4 className="gds-text--header-sm gds-text--bold -block text--white">[Report Subtitle] goes here</h4>
+                            </div>
+                            <div className="gds-layout__column--md-4 gds-layout__hidden-sm-down -text-right">
+                                <img className="img--full-width" src="https://c.gumgum.com/ads/com/mgmm/img/svg/gg-vi--color.svg" alt="GumGum Visual Intelligence" />
+                            </div>
+                        </div>
+                    </div>
                 </section>
+
+                {/* About this report section, needs [description] */}
+                <section className="-p-v-6 -p-h-3 bg--white">
+                    <div className="gds-layout__container">
+                        <div className="gds-layout__row">
+                            <div className="gds-layout__column--sm-12 -text-center--sm -m-b-3">
+                                <h3 className="gds-text--header-sm gds-text--bold -m-b-3 text--uppercase">About this report</h3>
+                                <p>[Description] goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat ornare tortor nec rutrum. Fusce accumsan ex felis, pretium ornare risus lobortis vel. Vivamus at enim eleifend, hendrerit turpis eget, volutpat lorem. In molestie felis at massa posuere fermentum. Maecenas quam leo, dapibus at massa vitae, ornare venenatis lacus. Vivamus massa mi, aliquet vel pharetra at, pellentesque ut lacus. Phasellus convallis, tellus a dignissim vehicula, ligula sapien fermentum massa, sit amet consequat justo odio vitae mauris. Curabitur pretium varius varius.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* Section 1 header, needs [category name] */}
+                <section className="-p-a-4 bg--color-1 text--white">
+                    <div className="gds-layout__container">
+                        <div className="gds-layout__row">
+                            <div className="gds-layout__column--sm-12 -text-center">
+                                <h3 className="gds-text--header-md gds-text--bold text--uppercase">[Category name] goes here</h3>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                {/* Section 1 content, needs [graph content], & [analysis] */}
+                <section className="-p-a-4">
+                    <div className="gds-layout__container">
+                        <div className="gds-layout__row">
+                            <div className="gds-layout__column--md-8">
+                                <p>[Graph] goes here</p>
+                            </div>
+                            <div className="gds-layout__column--md-4">
+                                <h3 className="gds-text--header-sm gds-text--bold text--uppercase -m-b-2">Analysis</h3>
+                                <p>[Analysis] goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat ornare tortor nec rutrum. Fusce accumsan ex felis, pretium ornare risus lobortis vel. Vivamus at enim eleifend, hendrerit turpis eget, volutpat lorem.</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* Footer logo section, doesn't need any data */}
+                <section className="-p-a-4 bg--color-1">
+                    <div className="gds-layout__container">
+                        <div className="gds-layout__row">
+                            <div className="gds-layout__column--sm-12 -text-center">
+                                <img className="footer-logo" src="https://c.gumgum.com/ads/com/mgmm/img/svg/gg-vi--white.svg" alt="GumGum Visual Intelligence" />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                {/* End of sections */}
             </div>
         );
     }
